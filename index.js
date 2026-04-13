efda01d67ad5c5765af85d4a952a0f148ddf0ade
Omar <omarino@elshorbagy.it>
2026-04-13 12:07:51 +0200

    Switch theme button

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
-async function setHTML(filePath, element) {
+async function setHTML(element, filePath) {
   try {
     const response = await fetch(filePath);
     const htmlContent = await response.text();
 
     element.innerHTML = htmlContent;
   } catch (error) {
-    console.error('Error loading file:', error);
+    console.error('Error injecting HTML:', error);
   }
 }
 
 async function injectHTML(event) {
-  console.log(event);
-
-  const file = event?.target?.attributes.getNamedItem("href")?.value;
+  const path = event?.target?.attributes.getNamedItem("href")?.value;
   const contentDiv = document.getElementById("content");
-
-  console.log(file);
-  console.log(contentDiv);
-
-  setHTML(file, contentDiv);
+  console.log(path);
+  setHTML(contentDiv, path);
 }
+
+function switchTheme() {
+  const htmlVariables = document.documentElement;
+  const currentTheme = htmlVariables.getAttribute("data-theme");
+  htmlVariables.setAttribute("data-theme", currentTheme == "dark" ? "light" : "dark");
+}
+
+// Source - https://stackoverflow.com/a/57795495
+// Posted by Mark Szabo, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-04-13, License - CC BY-SA 4.0
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+  const newColorScheme = event.matches ? "dark" : "light";
+  document.documentElement.setAttribute("data-theme", newColorScheme);
+});
+
 
 window.onload = (event) => {
   console.log('Hello!\n');
 
   const links = document.getElementsByClassName('section');
-
-  setHTML("/home.html", document.getElementById("content"));
-
+  setHTML(document.getElementById("content"), "/home.html");
   for (const link of links) {
     link.addEventListener('click', injectHTML);
   }
